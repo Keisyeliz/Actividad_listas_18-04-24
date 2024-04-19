21344a9e3aeaf619bab3ce5dee49443e9882f041
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-04-18 19:55:06 -0500

    Update lista.py

--- lista.py
+++ lista.py
@@ -14,23 +14,22 @@ def fnt_limpiar():
     os.system('cls')
     print('Autor: Keisy Murillo')
     print('Fecha: 18 de abril del 2024\n\n')
-    
-    
-def fnt_registrar(nombre, id , correo, contacto, edad, a_experiencia, profesion, ciudad, sexo):
-    if nombre == '' or id == '' or correo == '' or contacto or edad == '' or a_experiencia == '' or profesion == '' or ciudad == '' or sexo == '':
+
+def fnt_registrar(nombre, id, correo, contacto, edad, a_experiencia, profesion, ciudad, sexo):
+    if not (nombre and id and correo and contacto and edad and a_experiencia and profesion and ciudad and sexo):
         enter = input('\nError, debe de llenar todos los datos <ENTER>')
-    else: 
+    else:
         list_nombre.append(nombre)
         list_id.append(id)
         list_correo.append(correo)
         list_contacto.append(contacto)
         list_edad.append(edad)
-        list_a_experiencia(a_experiencia)
+        list_a_experiencia.append(a_experiencia)
         list_profesion.append(profesion)
         list_ciudad.append(ciudad)
         list_sexo.append(sexo)
         enter = input('\nRegistro exitoso <ENTER>')
-                        
+
 def fnt_selector(op):
     if op == '1':
         fnt_limpiar()
@@ -42,26 +41,23 @@ def fnt_selector(op):
             correo = input('Correo: ')
             contacto = input('Contacto: ')
             edad = input('Edad: ')
-            a_experiencia = input('Años de experiencia: ')
-            profesion = input('Profesión \n1.Ing Sistema\n2. Ing Informatico\n-> ')
+            a_experiencia = int(input('Años de experiencia: '))
+            profesion = input('Profesión (Ing. Sistema/Ing. Informatico): ')
             ciudad = input('Ciudad: ')
             sexo = input('Sexo(M/F): ')
-            if a_experiencia >= 2 and a_experiencia <= 4:
-                fnt_registrar(nombre, id , correo, contacto, edad, a_experiencia, profesion, ciudad, sexo)    
+            if 2 <= a_experiencia <= 4:
+                fnt_registrar(nombre, id, correo, contacto, edad, a_experiencia, profesion, ciudad, sexo)
             else:
-                enter = input('\nNo cuentas con los años de experiencia necesarios para ser candidato<ENTER>')
-            
-    if op == '2':
+                enter = input('\nNo cuentas con los años de experiencia necesarios para ser candidato a este trabajo <ENTER>')
+    elif op == '2':
         fnt_limpiar()
         for i in range(len(list_id)):
-            print(f'{list_id[i]} {list_nombre[i]} {list_correo[i]} {list_contacto[i]} {list_edad[i]} {list_a_experiencia[i]} {list_profesion[i]}  {list_ciudad[i]}  {list_sexo[i]} ')
-        enter = input('\Presione <ENTER> para continuar')
-        
-    if op == '3':
+            print(f'{list_id[i]} {list_nombre[i]} {list_correo[i]} {list_contacto[i]} {list_edad[i]} {list_a_experiencia[i]} {list_profesion[i]} {list_ciudad[i]} {list_sexo[i]}')
+        enter = input('\nPresione <ENTER> para continuar')
+    elif op == '3':
         sw = False
 
-
-while sw == True:
+while sw:
     fnt_limpiar()
-    opcion = input('\n\n ----- MENÚ PRINCIPAL -----\n1. Regristar\n2.Consultar\n3.Salir\n.-> ')
+    opcion = input('\n\n -----MENÚ-----\n1. Regristar\n2.Consultar\n3.Salir\n.-> ')
     fnt_selector(opcion)
